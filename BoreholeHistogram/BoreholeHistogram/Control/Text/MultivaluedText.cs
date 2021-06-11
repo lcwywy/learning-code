38a5c79c793e485d1ecef4ef1ab0b39c2b7c2c49
licong@southzn.com <licong@southzn.com>
2021-06-11 13:37:09 +0800

    保存

---
+++ BoreholeHistogram/BoreholeHistogram/Control/Text/MultivaluedText.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoreholeHistogram.Control.Text
+{
+    class MultivaluedText
+    {
+        #region 属性
+
+        #endregion
+
+        #region 私有字段
+
+        #endregion
+
+        #region 构造函数
+
+        #endregion
+
+        #region 绘制多值文本
+
+        #endregion
+    }
+}
